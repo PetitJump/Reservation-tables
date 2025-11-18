b35120f735888c3958b163872b205b6e60d65c47
PetitJump <killianbonnin7@gmail.com>
2025-11-18 11:51:27 +0100

    Premier commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    int table[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    char rep[] = "oui";
+    
+    while (strcmp(rep, "oui") == 0){
+
+     }
+
+    return 0;
+}
