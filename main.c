1e554bf5381d35354612c177de3e482a8f283b85
PetitJump <killianbonnin7@gmail.com>
2025-11-18 12:09:31 +0100

    Amélioration du code (ne fonctionne pas)

--- main.c
+++ main.c
@@ -12,9 +12,15 @@ int mise_a_jour_tables(int tables, int choix){
 int main(){
     int table[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     char rep[] = "oui";
+    int choix_table;
 
     while (strcmp(rep, "oui") == 0){
+        afficher_tables(table);
+        printf("Quel table voulez vous prendre ? ");
+        scanf("%d", &choix_table);
 
+        mise_a_jour_tables(*table, choix_table);
+        
         printf("Voulez vous continuer ? (oui/non) : ");
         scanf("%s", rep);
      }
