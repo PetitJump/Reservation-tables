8c71df01b06a7398dbb4f2ed0e82baede7a68be1
PetitJump <killianbonnin7@gmail.com>
2025-11-18 12:01:04 +0100

    Modification de la structure

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+void afficher_tables(int tables){
+
+}
+
+int mise_a_jour_tables(int tables, int choix){
+
+}
 
 int main(){
     int table[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     char rep[] = "oui";
-    
+
     while (strcmp(rep, "oui") == 0){
 
+        printf("Voulez vous continuer ? (oui/non) : ");
+        scanf("%s", rep);
      }
 
     return 0;
